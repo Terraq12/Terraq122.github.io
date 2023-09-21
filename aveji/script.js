2478e30f96e31dfbe1eb421d8b5088b178b995c4
Terraq12 <evaeva20042323@mail.ru>
2023-09-21 13:39:37 +0300

    fhfghfg

---
+++ aveji/script.js
@@ -0,0 +1,28 @@
+let burg = document.querySelector('.burger');
+
+burg.addEventListener('click', menu);
+function menu() {
+    let menu = document.querySelector('#Bmenu')
+    let body = document.querySelector('body')
+    let burgS = document.querySelector('.burger span')
+
+
+    menu.style.display = (menu.style.display == 'none') ? 'block' : 'none';
+    burg.classList.toggle('active');
+
+    if (menu.style.display == 'block') {
+        body.classList.add('bg');
+        burgS.classList.add('active');
+
+
+    }
+    else {
+        body.classList.remove('bg');
+        burgS.classList.remove('active');
+
+
+    }
+}
+
+ 
+
